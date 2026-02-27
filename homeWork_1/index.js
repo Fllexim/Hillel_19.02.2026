85fd66aa689528c4699ca7c53c5b19145c6092dd
ArtemUlianov <artem.ulashka@gmail.com>
2026-02-27 16:59:48 +0700

    First homework done

---
+++ homeWork_1/index.js
@@ -0,0 +1,19 @@
+//Примитивные
+const first = 10 ;
+const second = "hello" ;
+const third = true ;
+let fourth ;
+const fifth = null;
+const sixth = Symbol() ;
+const seventh = 123n ;
+console.log(typeof first);
+console.log(typeof second);
+console.log(typeof third);
+console.log(typeof fourth);
+console.log(typeof fifth);
+console.log(typeof sixth);
+console.log(typeof seventh);
+//Обьектные
+const obj = {};
+console.log(typeof obj)
+
