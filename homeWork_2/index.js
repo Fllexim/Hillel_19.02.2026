2a80f47c42a9094976b68bff10e0799d053b2110
ArtemUlianov <artem.ulashka@gmail.com>
2026-02-27 17:32:01 +0700

    Second homework done

---
+++ homeWork_2/index.js
@@ -0,0 +1,5 @@
+const userName = prompt("Enter your name please:");
+const userAge = prompt("Enter your age please:");
+const userSex = prompt("Enter your sex please:");
+
+console.log(`Ім'я користувача: ${userName}. Його вік: ${userAge}. Стать: ${userSex}`);
