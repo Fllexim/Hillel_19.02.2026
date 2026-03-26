e9d143777d5c350ebfbed07b603fe4fd1323d473
Artem Ulianov <artemulianov@Artems-MacBook-Air.local>
2026-03-26 19:31:33 +0700

    HomeWord 10 done

--- homeWork_10.1/index.js
+++ homeWork_10.1/index.js
@@ -1,18 +1,37 @@
-let ladder = {
-  step: 0,
+let company = {
+  sales: [
+    { name: `John`, salary: 1000 },
+    { name: `Alice`, salary: 600 },
+  ],
+  development: {
+    web: [
+      { name: `Peter`, salary: 2000 },
+      { name: `Alex`, salary: 1000 },
+    ],
+    internals: [
+      { name: `Jack`, salary: 1300 }
+    ],
+  },
+};
 
-  up: function () {
-    this.step += 1;
-    return this;
-  },
-  down: function () {
-    this.step -= 1;
-    return this;
-  },
-  showStep: function () {
-    return this.step;
-  },
+function sumTotalSalary(department) {
+  if (Array.isArray(department)) {
+    let sum = 0;
+
+    for (const person of department) {
+      sum += person.salary;
+    }
+
+    return sum;
+  }
+
+  let sum = 0;
+
+  for (const key in department) {
+    sum += sumTotalSalary(department[key]);
+  }
+
+  return sum;
 }
 
-console.log(ladder.up().up().down().showStep())
-
+console.log(sumTotalSalary(company));
