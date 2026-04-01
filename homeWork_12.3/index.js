cb173a0be3119bf345a7ef80eb2ca380d2945e43
Artem Ulianov <artemulianov@Artems-MacBook-Air.local>
2026-04-01 20:43:59 +0700

    Upgrade homework 12

--- homeWork_12.3/index.js
+++ homeWork_12.3/index.js
@@ -1,36 +1,22 @@
-const div = document.createElement("div");
+const div = document.querySelector("div");
+const btnAdd = document.querySelector("button");
+const input = document.querySelector("input");
 const list = document.createElement("ul");
-const btnAdd = document.createElement("button");
-const input = document.createElement("input");
 
-input.classList.add("input");
-input.placeholder = "Додати нове завдання";
-btnAdd.textContent = "Додати";
-btnAdd.classList.add("btnAdd");
-
-document.body.appendChild(div);
-div.appendChild(input);
-div.appendChild(btnAdd);
 div.appendChild(list);
 
-div.addEventListener("click", (e) => {
-  if (e.target.classList.contains("btnAdd")) {
-    if (input.value.trim()) {
-      const text = input.value.trim();
-      const li = document.createElement("li");
-      const btnDelete = document.createElement("button");
+btnAdd.addEventListener("click", () => {
+  const li = document.createElement("li");
+  const btnDelete = document.createElement("button");
 
-      btnDelete.textContent = "Видалити";
-      btnDelete.classList.add("btnDelete");
+  btnDelete.textContent = "Видалити";
+  li.textContent = input.value + " ";
 
-      li.append(text, " ", btnDelete);
-      list.appendChild(li);
-      input.value = "";
-    } else {
-      alert("Введіть текст");
-    }
-  }
-  if (e.target.classList.contains("btnDelete")) {
-    e.target.closest("LI").remove();
-  }
+  list.appendChild(li);
+  li.append(btnDelete);
+  input.value = "";
 });
+
+list.addEventListener("click", (e) => {
+  e.target.closest('LI').remove();
+})
