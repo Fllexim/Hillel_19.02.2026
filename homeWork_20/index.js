196146bb308ceeb59718d0262c3f4974883b4634
Artem Ulianov <artemulianov@Artems-MacBook-Air.local>
2026-05-10 07:36:48 +0700

    Homework 20 upgrade 2.0

--- homeWork_20/index.js
+++ homeWork_20/index.js
@@ -3,6 +3,7 @@
 const input = $(".form-control");
 const form = $("form");
 const list = $("ul");
+const taskModal = $("#taskModal");
 
 // Array for LocalStorage
 
@@ -23,7 +24,9 @@ function addItemsInDOM() {
     const span = $("<span>");
     const button = $("<button>");
 
-    li.addClass("d-flex justify-content-between align-items-center mt-2 rounded-2 bg-light p-2");
+    li.addClass(
+      "d-flex justify-content-between align-items-center mt-2 rounded-2 bg-light p-2",
+    );
     li.attr("data-id", item.id);
 
     checkbox.attr("type", "checkbox").prop("checked", item.isDone);
@@ -67,31 +70,51 @@ form.on("submit", (event) => {
 
 // Checkbox
 
-list.on("change", (event) => {
-  if (event.target.type === "checkbox") {
-    const li = $(event.target).closest("li");
-    const spanText = li.find("span");
+function saveCheckbox(li) {
+  const spanText = li.find("span");
+  const id = li.data("id");
+  const item = arrayTodos.find((item) => item.id === id);
+  item.isDone = event.target.checked;
+  saveTodos();
 
-    const id = li.data("id");
-    const item = arrayTodos.find((item) => item.id === id);
-    item.isDone = event.target.checked;
-    saveTodos();
-
-    spanText.toggleClass("text-decoration-line-through", event.target.checked);
-  }
-});
+  spanText.toggleClass("text-decoration-line-through", event.target.checked);
+}
 
 // Button delete
 
-list.on("click", (event) => {
-  if ($(event.target).is("button")) {
-    const li = $(event.target).closest("li");
-    const id = li.data("id");
+function deleteTask(li) {
+  const id = li.data("id");
+  arrayTodos = arrayTodos.filter((item) => item.id !== id);
 
-    arrayTodos = arrayTodos.filter((item) => item.id !== id);
-    saveTodos();
-    li.remove();
+  saveTodos();
+  li.remove();
+}
+
+// Open openModa
+
+function openModal(li) {
+  const modal = new bootstrap.Modal(taskModal);
+
+  modal.show();
+}
+
+// ON list
+
+list.on("click", (event) => {
+  const target = $(event.target);
+  const li = target.closest("li");
+
+  if (target.is("button")) {
+    deleteTask(li);
+    return;
   }
+
+  if (target.attr("type") === "checkbox") {
+    saveCheckbox(li);
+    return;
+  }
+
+  openModal(li);
 });
 
 addItemsInDOM();
