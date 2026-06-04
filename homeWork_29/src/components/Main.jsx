4f10b51d7623c5e382c1b70d984072a612c3d66a
Artem Ulianov <artemulianov@Artems-MacBook-Air.local>
2026-06-04 20:55:50 +0700

    Homework 29 up

--- homeWork_29/src/components/Main.jsx
+++ homeWork_29/src/components/Main.jsx
@@ -3,7 +3,6 @@ import selectors from "../redux/selectors";
 
 const Main = () => {
   const value = useSelector(selectors.counter.value);
-  console.log(value);
 
   return <span>Votes: {value}</span>
 };
