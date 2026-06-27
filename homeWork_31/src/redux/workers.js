6ca4cdfaf157f00a822723b13b89fd11b3d985b3
Artem Ulianov <artemulianov@Artems-MacBook-Air.local>
2026-06-27 12:43:14 +0700

    Home 32 upgd

--- homeWork_31/src/redux/workers.js
+++ homeWork_31/src/redux/workers.js
@@ -16,71 +16,87 @@ export function* loadTodosWorker() {
 }
 
 export function* addTodoWorker(action) {
-  const response = yield call(fetch, API, {
-    method: "POST",
+  try {
+    const response = yield call(fetch, API, {
+      method: "POST",
 
-    headers: {
-      "Content-Type": "application/json",
-    },
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-    body: JSON.stringify({
-      text: action.payload,
-      completed: false,
-    }),
-  });
+      body: JSON.stringify({
+        text: action.payload,
+        completed: false,
+      }),
+    });
 
-  const data = yield call([response, response.json]);
+    const data = yield call([response, response.json]);
 
-  yield put(todo.actions.addTodo(data));
+    yield put(todo.actions.addTodo(data));
+  } catch (error) {
+    console.error("Add todo error:", error);
+  }
 }
 
 export function* toggleTodoWorker(action) {
-  const response = yield call(fetch, `${API}/${action.payload.id}`, {
-    method: "PUT",
+  try {
+    const response = yield call(fetch, `${API}/${action.payload.id}`, {
+      method: "PUT",
 
-    headers: {
-      "Content-Type": "application/json",
-    },
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-    body: JSON.stringify({
-      ...action.payload,
-      completed: !action.payload.completed,
-    }),
-  });
+      body: JSON.stringify({
+        ...action.payload,
+        completed: !action.payload.completed,
+      }),
+    });
 
-  const data = yield call([response, response.json]);
+    const data = yield call([response, response.json]);
 
-  yield put(todo.actions.updateTodo(data));
+    yield put(todo.actions.updateTodo(data));
+  } catch (error) {
+    console.error("Toggle todo error:", error);
+  }
 }
 
 export function* editTodoWorker(action) {
-  const response = yield call(fetch, `${API}/${action.payload.id}`, {
-    method: "PUT",
+  try {
+    const response = yield call(fetch, `${API}/${action.payload.id}`, {
+      method: "PUT",
 
-    headers: {
-      "Content-Type": "application/json",
-    },
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-    body: JSON.stringify({
-      ...action.payload,
-    }),
-  });
+      body: JSON.stringify({
+        ...action.payload,
+      }),
+    });
 
-  const data = yield call([response, response.json]);
+    const data = yield call([response, response.json]);
 
-  yield put(todo.actions.updateTodo(data));
+    yield put(todo.actions.updateTodo(data));
+  } catch (error) {
+    console.error("Edit todo error:", error);
+  }
 }
 
 export function* clearTodosWorker() {
-  const response = yield call(fetch, API);
+  try {
+    const response = yield call(fetch, API);
 
-  const todos = yield call([response, response.json]);
+    const todos = yield call([response, response.json]);
 
-  for (const item of todos) {
-    yield call(fetch, `${API}/${item.id}`, {
-      method: "DELETE",
-    });
+    for (const item of todos) {
+      yield call(fetch, `${API}/${item.id}`, {
+        method: "DELETE",
+      });
+    }
+
+    yield put(todo.actions.clearTodos());
+  } catch (error) {
+    console.error("Clear todos error:", error);
   }
-
-  yield put(todo.actions.clearTodos());
 }
